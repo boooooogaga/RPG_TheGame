d25dcc2490ef46e86602b418ad3764ff342fde94
boo_ga_ga_ <vasyapetya233@gmail.com>
2025-10-13 21:39:47 +0300

    0.0.1
    - Basic logic off movement
    - Add knife test sprite

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+     private const float gravityScale = 9.8f,
+                  speed = 8f,
+                  turnSpeed = 90f;
+    private float mouseX = 0f, VerticalSpeed = 0f;
+
+    private CharacterController characterController;
+    [SerializeField] Camera GoCamera;
+
+    private void CameraRotation()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(new Vector3(0f, mouseX * turnSpeed * Time.deltaTime, 0f));
+    }
+    private void MoveCharacter()
+    {
+        Vector3 velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        velocity = transform.TransformDirection(velocity) * speed; // переводим из глобальной системі координат в локальную
+
+
+
+        if (characterController.isGrounded) VerticalSpeed = 0f;
+        else
+        {
+            VerticalSpeed -= gravityScale * Time.deltaTime;
+            velocity.y = VerticalSpeed;
+        }
+
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        CameraRotation();
+        MoveCharacter();
+    }
+}
