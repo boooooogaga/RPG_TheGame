fe4178e2188efb050529065cf8b72e5bd793ccfe
boo_ga_ga_ <vasyapetya233@gmail.com>
2025-10-19 22:18:42 +0300

    0.0.1
    - Ready default sword attack mechanick

--- Scripts/Sword.cs
+++ Scripts/Sword.cs
@@ -1,31 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sword : MonoBehaviour
 {
-    SwordData data;
 
     BoxCollider attackCollider;
 
-    private void Awake()
+
+    [Header("UI Components")]
+    public Image swordUIImage; // картинка меча на экране
+
+    [Header("Sword Settings")]
+    public SwordData currentSword; // текущие данные меча
+
+    private bool canAttack = true;
+
+    private void Start()
     {
         attackCollider = GetComponent<BoxCollider>();
-
         attackCollider.enabled = false;
     }
-
-    public IEnumerator Attack()
+    public void Attack()
     {
-        Debug.Log("Attack");
-        yield return new WaitForSeconds(data.attackDelay);
-        attackCollider.enabled = true;
-        yield return new WaitForSeconds(data.attackTime);
-        attackCollider.enabled = false;
+        if (!canAttack) return;
+        StartCoroutine(AttackRoutine());
     }
 
+    private IEnumerator AttackRoutine()
+    {
+        canAttack = false;
+
+        yield return new WaitForSeconds(currentSword.attackDelay);
+        Debug.Log($"Атака мечом: {currentSword.swordName}");
+        attackCollider.enabled = true;
+
+        // Задержка между ударами
+        yield return new WaitForSeconds(currentSword.attackSpeed);
+        attackCollider.enabled = false;
+        canAttack = true;
+    }
+
+    public void Equip(SwordData newSword)
+    {
+        currentSword = newSword;
+        swordUIImage.sprite = newSword.swordSprite;
+        Debug.Log($"Экипирован меч: {newSword.swordName}");
+    }
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(other.gameObject);
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
