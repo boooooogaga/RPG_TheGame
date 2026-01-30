720b984b4b71128fe1ef4670159d212051b82353
boo_ga_ga_ <156234871+boooooogaga@users.noreply.github.com>
2026-01-30 13:35:58 +0200

    Hand inerta add

---
+++ Scripts/PlayerScripts/HandInertia.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class HandInertia : MonoBehaviour
+{
+    public Transform cameraTransform;
+
+    [Header("Inertia settings")]
+    public float positionStrength = 0.05f;
+    public float rotationStrength = 2f;
+    public float smooth = 8f;
+
+    private Vector3 lastCamEuler;
+    private Vector3 targetPos;
+    private Quaternion targetRot;
+
+    private Vector3 startLocalPos;
+    private Quaternion startLocalRot;
+
+    void Start()
+    {
+        startLocalPos = transform.localPosition;
+        startLocalRot = transform.localRotation;
+        lastCamEuler = cameraTransform.eulerAngles;
+    }
+
+    void Update()
+    {
+        Vector3 camDelta = cameraTransform.eulerAngles - lastCamEuler;
+
+        // фиксим скачки 0 → 360
+        camDelta.x = Mathf.DeltaAngle(0, camDelta.x);
+        camDelta.y = Mathf.DeltaAngle(0, camDelta.y);
+
+        // позиционная инерция
+        targetPos = startLocalPos +
+                    new Vector3(
+                        -camDelta.y * positionStrength,
+                        -camDelta.x * positionStrength,
+                        0f
+                    );
+
+        // вращательная инерция
+        targetRot = startLocalRot *
+                    Quaternion.Euler(
+                        camDelta.x * rotationStrength,
+                        camDelta.y * rotationStrength,
+                        0f
+                    );
+
+        transform.localPosition = Vector3.Lerp(
+            transform.localPosition,
+            targetPos,
+            Time.deltaTime * smooth
+        );
+
+        transform.localRotation = Quaternion.Slerp(
+            transform.localRotation,
+            targetRot,
+            Time.deltaTime * smooth
+        );
+
+        lastCamEuler = cameraTransform.eulerAngles;
+    }
+}
